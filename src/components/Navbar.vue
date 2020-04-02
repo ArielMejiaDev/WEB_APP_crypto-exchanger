1d48afff4c15a20c6a10746c65456027ecf65413
ArielSalvadorDev <arielsalvadormejia@gmail.com>
2020-04-01 23:50:12 -0600

    wip

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,7 +4,7 @@
       <nav class="flex items-center justify-between flex-wrap bg-green-400 p-6">
         <div class="flex items-center flex-shrink-0 text-white mr-6">
           <Icon class="mr-2" />
-          <router-link :to="{name: 'Home'}" class="font-semibold text-xl tracking-tight">Cripto Exchanger</router-link>
+          <router-link :to="{name: 'Home'}" class="font-semibold text-xl tracking-tight">Crypto Exchanger</router-link>
         </div>
         <div class="hidden sm:block w-full block flex-grow lg:flex lg:items-center lg:w-auto">
           <div class="text-sm lg:flex-grow">
